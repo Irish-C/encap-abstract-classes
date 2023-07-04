10b1eab88bbe7a546c14773530f38cc72283e5bc
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-05 03:13:36 +0800

    Create instance methods for Pet class

--- Pet class/Pet.py
+++ Pet class/Pet.py
@@ -5,3 +5,27 @@ class Pet:
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
+
+    # a method that sets the pet's name
+    def set_name():
+        pass
+
+    # a method that sets what type of animal
+    def set_animal_type():
+        pass
+
+    # a method that sets the pet's age
+    def set_age():
+        pass
+
+    # a method that returns the pet's name
+    def get_name():
+        pass
+
+    # a method that returns what type of animal
+    def get_animal_type():
+        pass
+
+    # a method that returns the pet's age
+    def get_age():
+        pass
