32d42a623e115a87de914704cd14a3c64e8cb6d6
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-04 19:20:31 +0800

    Add test program for the two objects

--- TestFan.py
+++ TestFan.py
@@ -5,3 +5,15 @@ class TestFan:
         # create two objects
         fan1 = Fan()
         fan2 = Fan()
+
+        # sets fan speed to maximum, radius to 10, color to yellow, and turn on
+        fan1.set_speed(Fan.FAST)
+        fan1.set_radius(10)
+        fan1.set_color('yellow')
+        fan1.turn_on(True)
+
+        # sets fan speed to maximum, radius to 10, color to yellow, and turn on
+        fan1.set_speed(Fan.FAST)
+        fan1.set_radius(5)
+        fan1.set_color('blue')
+        fan1.turn_on(False)
