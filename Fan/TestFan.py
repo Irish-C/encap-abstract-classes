ed7c8149138827f1bbbac364c5b5e7418202e97e
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-04 21:08:10 +0800

    Add 'max' in fan1

--- Fan/TestFan.py
+++ Fan/TestFan.py
@@ -29,7 +29,7 @@ class TestFan:
         # Place fan1 and fan2 into the TestFan layout
         TestFan_layout = [
             [sg.Text(title.center(50), font=('Elephant', 25), justification='center')],
-            [sg.Frame('', [[sg.Text(f"  Fan 1 is turned {'On' if fan1.get_on() else 'Off'} \n\n  speed: {fan1.get_speed()} \n  radius: {fan1.get_radius()} \n  color: {fan1.get_color()}\n\n\n", size=(50,5), font=('Lucida',12))]], background_color='Light Green',border_width=3)], 
+            [sg.Frame('', [[sg.Text(f"  Fan 1 is turned {'On' if fan1.get_on() else 'Off'} \n\n  speed: {fan1.get_speed()} [max] \n  radius: {fan1.get_radius()} \n  color: {fan1.get_color()}\n\n\n", size=(50,5), font=('Lucida',12))]], background_color='Light Green',border_width=3)], 
             [sg.Frame('', [[sg.Text(f"  Fan 2 is turned {'On' if fan2.get_on() else 'Off'} \n\n  speed: {fan2.get_speed()} \n  radius: {fan2.get_radius()} \n  color: {fan2.get_color()}\n\n\n", size=(50,5), font=('Lucida',12))]], background_color='Pink',border_width=3)], 
         ]
 
