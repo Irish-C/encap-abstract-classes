e5be7a29e885f84e168ed9e36237f2a148b79b39
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-05 03:41:45 +0800

    Create input feature for pet name

--- Pet class/TestPet.py
+++ Pet class/TestPet.py
@@ -10,7 +10,13 @@ class TestPet:
     def __init__(self):
         self.pet = Pet()
 
-        # Create window
+        # Create TestPet window
         self.root = tk.Tk()
         self.root.title('My Pet Profile')
         self.root.geometry('300x250')
+
+        # An input feature for the pet's name
+        self.pet_label = ttk.Label(self.root, text="Pet Name:")
+        self.pet_label.pack()
+        self.pet_entry = ttk.Entry(self.root)
+        self.pet_entry.pack()
