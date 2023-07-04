62963137bf743b58bed7fc95e20e13293a64ded3
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-04 21:23:40 +0800

    Create instance methods of Car class

--- Car class/Car.py
+++ Car class/Car.py
@@ -4,3 +4,16 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
+
+
+    # a method that add 5 each time its called
+    def accelerate():
+        pass
+
+    # a method that subtract 5 each time its called
+    def brake():
+        pass
+    
+    # a method that returns the current speed
+    def get_speed():
+        pass
