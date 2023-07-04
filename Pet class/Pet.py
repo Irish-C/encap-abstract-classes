ac4102ba491b1e7a06e4cdd82a0dc11e8ce43809
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-05 03:15:21 +0800

    Write instances of all Mutator methods

--- Pet class/Pet.py
+++ Pet class/Pet.py
@@ -7,16 +7,16 @@ class Pet:
         self.__age = age
 
     # a method that sets the pet's name
-    def set_name():
-        pass
-
+    def set_name(self, name):
+        self.__name = name
+        
     # a method that sets what type of animal
-    def set_animal_type():
-        pass
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
 
     # a method that sets the pet's age
-    def set_age():
-        pass
+    def set_age(self, age):
+        self.__age = age
 
     # a method that returns the pet's name
     def get_name():
