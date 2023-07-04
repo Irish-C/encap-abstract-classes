15436164db0745fdb6a91ef2a4a1b0581d46edf6
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-04 23:16:17 +0800

    Create a current speed method

--- Car class/TestCar.py
+++ Car class/TestCar.py
@@ -1,6 +1,15 @@
 # import Car module
 from Car import Car
 import tkinter as tk
+import time
+
+# Create TestCar class
+class TestCar:
+
+    # a method that returns a current speed
+    def update_car_speed(self):
+        current_speed = car1.get_speed()
+        current_speed.config(text=f"Current Speed: {current_speed} mph")
 
 # Create a car object
 car1 = Car(2017, 'Volkswagen')
