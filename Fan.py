0197b5b0c99419c64fad934d6e6df06042eaa786
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-04 18:09:08 +0800

    Add Mutator methods for all data field

--- Fan.py
+++ Fan.py
@@ -11,15 +11,27 @@ class Fan:
         self.__radius = radius
         self.__color = color
 
-    # Accesor methods (Getter)
+    # Accesor(Getter) and Mutator (Setter) methods
     def get_speed(self):
+        return self.__speed
+    
+    def set_speed(self, speed):
         return self.__speed
     
     def get_on(self):
         return self.__on
     
+    def set_on(self, on):
+        return self.__on
+    
     def get_radius(self):
         return self.__radius
     
+    def set_radius(self, radius):
+        return self.__radius
+    
     def get_color(self):
+        return self.__color
+    
+    def set_color(self):
         return self.__color
