8eade93a01feacd4584709f52586d74eb6b63876
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-05 03:08:42 +0800

    Create Pet class and its constructor

--- Car class/main.py
+++
@@ -1,6 +0,0 @@
-from TestCar import TestCar
-
-test_car = TestCar()
-test_car.car1_acceleration()
-test_car.car1_brake()
-test_car.root.mainloop()
---
+++ Pet class/Pet.py
@@ -0,0 +1,7 @@
+# Pet class
+class Pet:
+    # constructor
+    def __init__(self, name, animal_type, age):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
