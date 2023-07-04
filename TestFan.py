69e16fb9d40c17bba96e6eb8ff29fba640e868c4
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-04 19:28:01 +0800

    Correction from fan1. to fan2.

--- TestFan.py
+++ TestFan.py
@@ -6,14 +6,14 @@ class TestFan:
         fan1 = Fan()
         fan2 = Fan()
 
-        # sets fan speed to maximum, radius to 10, color to yellow, and turn on
+        # sets fan1 speed to maximum, radius to 10, color to yellow, and turn on
         fan1.set_speed(Fan.FAST)
         fan1.set_radius(10)
         fan1.set_color('yellow')
         fan1.turn_on(True)
 
-        # sets fan speed to maximum, radius to 10, color to yellow, and turn on
-        fan1.set_speed(Fan.FAST)
-        fan1.set_radius(5)
-        fan1.set_color('blue')
-        fan1.turn_on(False)
+        # sets fan2 speed to maximum, radius to 10, color to yellow, and turn on
+        fan2.set_speed(Fan.FAST)
+        fan2.set_radius(5)
+        fan2.set_color('blue')
+        fan2.turn_on(False)
