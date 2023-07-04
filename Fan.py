e1d6ae40f1cc8d7165944277bb3702be4da2a864
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-04 17:57:48 +0800

    Add Accesor methods for all data field

--- Fan.py
+++ Fan.py
@@ -10,3 +10,16 @@ class Fan:
         self.__on = on
         self.__radius = radius
         self.__color = color
+
+    # Accesor methods (Getter)
+    def get_speed(self):
+        return self.__speed
+    
+    def get_on(self):
+        return self.__on
+    
+    def get_radius(self):
+        return self.__radius
+    
+    def get_color(self):
+        return self.__color
