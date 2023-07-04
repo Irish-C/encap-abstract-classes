de252dbd14b8311219d836012362edc777a51a36
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-04 23:36:54 +0800

    Create loading bar method

--- Car class/TestCar.py
+++ Car class/TestCar.py
@@ -2,13 +2,16 @@
 from Car import Car
 import tkinter as tk
 
-# Create TestCar class
-class TestCar:
+# a method that returns a current speed
+def update_car_speed():
+    current_speed = car1.get_speed()
+    current_speed.config(text=f"Current Speed: {current_speed} mph")
 
-    # a method that returns a current speed
-    def update_car_speed(self):
-        current_speed = car1.get_speed()
-        current_speed.config(text=f"Current Speed: {current_speed} mph")
+# a method that updates loading bar
+def update_loading_bar(value):
+    global loading_bar
+    loading_bar['value'] = value
+
 
 # Create a car object
 car1 = Car(2017, 'Volkswagen')
